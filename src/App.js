574536b412d878ef7bf00edd031464139f675d38
muriithi-ian <imuriithiian@gmail.com>
2021-04-15 14:54:40 +0300

    added interaction with indexDb: add, update and delete items

--- src/App.js
+++ src/App.js
@@ -1,30 +1,79 @@
-import './App.css'
+import './App.css';
+import Dexie from 'dexie';
+import { useLiveQuery } from "dexie-react-hooks";
+
+
 
 const App = () => {
+
+  const db = new Dexie('MarketList');
+  db.version(1).stores(
+    { items: "++id, name, price, itemHasBeenPurchased" }
+  )
+
+  const allItems = useLiveQuery(() => db.items.toArray(), []);
+  if (!allItems) return null;
+
+  const addItemToDb = async event => {
+    event.preventDefault();
+    const name = document.querySelector('.item-name').value
+    const price = document.querySelector('.item-price').value
+
+    await db.items.add({
+      name,
+      price: Number(price),
+      itemHasBeenPurchased: false
+    })
+  }
+
+  const removeItemFromDb = async id => {
+    await db.items.delete(id)
+  }
+
+  const markAsPurchased = async (id, event) => {
+    if (event.target.checked) {
+      await db.items.update(id, { itemHasBeenPurchased: true })
+    } else {
+      await db.items.update(id, { itemHasBeenPurchased: false })
+    }
+  }
+
+  const itemData = allItems.map(({ id, name, price, itemHasBeenPurchased }) => (
+    <div className="row" key={id}>
+      <p className="col s5">
+        <label>
+          <input
+            type="checkbox"
+            checked={itemHasBeenPurchased}
+            onChange={event => markAsPurchased(id, event)}
+          />
+          <span className="black-text">{name}</span>
+        </label>
+      </p>
+      <p className="col s5">${price}</p>
+      <i onClick={() => removeItemFromDb(id)} className="col s2 material-icons delete-button">
+        delete
+      </i>
+    </div>
+  ))
+
   return (
     <div className="container">
       <h3 className="green-text center-align">Market List App</h3>
-      <form className="add-item-form">
-        <input type="text" placeholder="Name of item" required />
-        <input type="number" placeholder="Price in USD" required />
+      <form className="add-item-form" onSubmit={event => addItemToDb(event)} >
+        <input type="text" className="item-name" placeholder="Name of item" required />
+        <input type="number" step=".01" className="item-price" placeholder="Price in USD" required />
         <button type="submit" className="waves-effect waves-light btn right">Add item</button>
       </form>
-      <div className="card white darken-1">
-        <div className="card-content">
-          <form action="#">
-            <div className="row">
-              <p className="col s5">
-                <label>
-                  <input type="checkbox" />
-                  <span className="black-text">Tomato</span>
-                </label>
-              </p>
-              <p className="col s5">$5</p>
-              <i className="col s2 material-icons delete-button">delete</i>
-            </div>
-          </form>
+      {allItems.length > 0 &&
+        <div className="card white darken-1">
+          <div className="card-content">
+            <form action="#">
+              {itemData}
+            </form>
+          </div>
         </div>
-      </div>
+      }
     </div>
   );
 }
